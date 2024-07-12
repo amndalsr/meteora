a4748084f905511b795155f24cc3c20fc9b3ee71
amndalsr <laisamanda2015@gmail.com>
2024-07-12 16:49:29 -0300

    usando reduce

--- src/context/CarrinhoContext.jsx
+++ src/context/CarrinhoContext.jsx
@@ -2,12 +2,23 @@ import { createContext, useState } from "react";
 
 export const CarrinhoContext = createContext();
 
-export const CarrinhoProvider = ({children}) => {
-    const [carrinho, setCarrinho] = useState([]);
+export const CarrinhoProvider = ({ children }) => {
+  const [carrinho, setCarrinho] = useState([]);
+  const [quantidade, setQuatidade] = useState(0);
+  const [valorTotal, setValorTotal] = useState(0);
 
-    return (
-        <CarrinhoContext.Provider value={{carrinho, setCarrinho}}>
-            {children}
-        </CarrinhoContext.Provider>
-    )
-}
+  return (
+    <CarrinhoContext.Provider
+      value={{
+        carrinho,
+        setCarrinho,
+        quantidade,
+        setQuatidade,
+        valorTotal,
+        setValorTotal,
+      }}
+    >
+      {children}
+    </CarrinhoContext.Provider>
+  );
+};
